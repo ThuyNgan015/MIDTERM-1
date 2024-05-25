c94e3f53185572419e587724eff35408f46da658
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-05-25 11:07:08 +0700

    exercise-5

--- exercise-5/src/App.jsx
+++ exercise-5/src/App.jsx
@@ -4,9 +4,13 @@ import { AVAILABLE_CART } from "./data.js";
 function App() {
   return (
   <>{/* Your code  here */}
-      {AVAILABLE_CART.map((cart, index) => (
-      <Place key={index} cart={cart} />
+      <header>
+      <h1>My Items</h1>
+    </header>
+      <li>{AVAILABLE_CART.map((cart, index) => (
+      <Cart key={index} cart={cart} />
     ))}
+    </li>
   </>
 );
 }
